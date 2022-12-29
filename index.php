c814ee27d00922856844392908af9766d1290212
Leonar Estupiñan <75337656+LeonarEQ@users.noreply.github.com>
2022-12-29 16:46:38 +0000

    Primer index

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include("persona.php");
+
+class Profesor extends Persona
+{
+    public string $curso;
+    function __construct($nombre, $apellidos, $edad, $curso)
+    {
+        parent::__construct($nombre, $apellidos, $edad);
+        $this->curso = $curso;
+    }
+    
+}
