97e782600dc302e46667db11092aa8621c43a8ba
Leonar Estupiñan <75337656+LeonarEQ@users.noreply.github.com>
2022-12-29 16:58:11 +0000

    creacion de borrar.php

---
+++ borrar.php
@@ -0,0 +1,17 @@
+<?php
+include("Profesor.php");
+session_start();
+if(isset($_GET["id"])){
+    if($_SESSION["profesores"]){
+        $id=$_GET["id"];
+        $profesores=$_SESSION["profesores"];
+        for ($i=0; i < count($profesores); $i++){
+            $profesor=$profesores[$i];
+            if($profesor->id==$id){
+                unset($profesor[$i]);
+
+            }
+        }
+    }
+}
+else{}
